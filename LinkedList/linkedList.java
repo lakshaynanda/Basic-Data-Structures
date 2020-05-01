e8dff5c2c95cde793cff0a4fec07845e973601de
lakshaynanda <lakshay.nanda08@gmail.com>
2020-05-01 12:39:02 +0530

    LinkedList Update 2

--- LinkedList/linkedList.java
+++ LinkedList/linkedList.java
@@ -23,7 +23,7 @@ public class linkedList
 
     public void addLast(int data)
     {
-        Node node=new Node();~
+        Node node=new Node();
 
         if(this.size==0)
         {
@@ -32,16 +32,100 @@ public class linkedList
 
             this.size++;
             this.head=this.tail=node;
+
+            return;
         }
         node.data=data;
         node.next=null;
         tail.next=node;
         tail=node;
         this.size++;
-
+    }
+    public void display()
+    {
+        Node temp=this.head;
+        while(temp!=null)
+        {
+            System.out.print(temp.data+" -> ");
+            temp=temp.next;
+        }System.out.println(".");
 
     }
-    public static void main(String[] args) {
+    public void addFirst(int data)
+    {
+        Node node=new Node();
+        if(this.size==0)
+        {
+            node.data=data;
+            node.next=null;
+            this.head=this.tail=node;
+            this.size++;
+            return;
+        }
+        node.data=data;
+        node.next=head;
+        head=node;
+
+        this.size++;
+    }
+    // public void addAtAny(int data,int i)
+    // {
+    //     Node node=new Node();
+    //     if(this.size==0)
+    //     {
+    //         node.data=data;
+    //         node.next=null;
+    //         this.head=this.tail=node;
+    //         this.size++;
+    //         return;
+    //     }
+    //     node.data=data;
+    //     node.next=
+    // }
+
+    public int getLast() throws Exception
+    {
+        if(this.size==0)
+        {
+            throw new Exception("Emtpy Bro");
+        }
+        return tail.data;
+    }
+    public int getFirst() throws Exception
+    {
+        if(this.size==0)
+        {
+            throw new Exception("Empty Bro");
+        }
+        return head.data;
+    }
+    public int getAt(int idx) throws Exception
+    {
+        if(this.size==0)
+        {
+            throw new Exception("Empty Bro");
+        }
+        else if(idx<0 || idx>=this.size)
+        {
+            throw new Exception("Out of Bounds");
+        }
+        Node node=head;
+        for(int i=0;i<idx;i++)
+        {
+            node=node.next;
+        }
+        return node.data;
+    }
+
+    public static void main(String[] args) throws Exception{
+        linkedList list=new linkedList();
         
+        list.addFirst(5);
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+        System.out.println(list.getAt(2));
+        list.display();
     }
 }
