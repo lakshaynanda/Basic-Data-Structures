3d54dd81581282c8eca7410ca7e67e76159a22b3
lakshaynanda <lakshay.nanda08@gmail.com>
2020-05-02 18:06:31 +0530

    LinkedList updatess

--- LinkedList/linkedList.java
+++ LinkedList/linkedList.java
@@ -1,8 +1,9 @@
 import java.util.*;
-public class linkedList
-{
-    public class Node
-    {
+
+import javax.lang.model.util.ElementScanner14;
+
+public class linkedList {
+    public class Node {
         int data;
         Node next;
     }
@@ -11,150 +12,185 @@ public class linkedList
     public Node tail;
     public int size;
 
-    public int size()
-    {
+    public int size() {
         return this.size;
     }
 
-    public boolean isEmpty()
-    {
-        return this.size==0;
+    public boolean isEmpty() {
+        return this.size == 0;
     }
 
-    public void addLast(int data)
-    {
-        Node node=new Node();
+    public void addLast(int data) {
+        Node node = new Node();
 
-        if(this.size==0)
-        {
-            node.data=data;
-            node.next=null;
+        if (this.size == 0) {
+            node.data = data;
+            node.next = null;
 
             this.size++;
-            this.head=this.tail=node;
+            this.head = this.tail = node;
 
             return;
         }
-        node.data=data;
-        node.next=null;
-        tail.next=node;
-        tail=node;
+        node.data = data;
+        node.next = null;
+        tail.next = node;
+        tail = node;
         this.size++;
     }
-    public void display()
-    {
-        Node temp=this.head;
-        while(temp!=null)
-        {
-            System.out.print(temp.data+" -> ");
-            temp=temp.next;
-        }System.out.println(".");
+
+    public void display() {
+        Node temp = this.head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println(".");
 
     }
-    public void addFirst(int data)
-    {
-        Node node=new Node();
-        if(this.size==0)
-        {
-            node.data=data;
-            node.next=null;
-            this.head=this.tail=node;
+
+    public void addFirst(int data) {
+        Node node = new Node();
+        if (this.size == 0) {
+            node.data = data;
+            node.next = null;
+            this.head = this.tail = node;
             this.size++;
             return;
         }
-        node.data=data;
-        node.next=head;
-        head=node;
+        node.data = data;
+        node.next = head;
+        head = node;
 
         this.size++;
     }
-    public void addAtAny(int data,int i) throws Exception
-    {
-        if(i<0 || i>this.size)
-        {
+
+    public void addAtAny(int data, int i) throws Exception {
+        if (i < 0 || i > this.size) {
             throw new Exception("Invalid");
-        }
-        else if(i==0)
-        {
+        } else if (i == 0) {
             this.addFirst(data);
-        }
-        else if(i==this.size)
-        {
+        } else if (i == this.size) {
             this.addLast(data);
-        }
-        else{
-            Node node=this.getNodeAt(i-1);
-            Node nodep1=node.next;
-            Node newNode=new Node();
-            newNode.data=data;
-            node.next=newNode;
-            newNode.next=nodep1;
+        } else {
+            Node node = this.getNodeAt(i - 1);
+            Node nodep1 = node.next;
+            Node newNode = new Node();
+            newNode.data = data;
+            node.next = newNode;
+            newNode.next = nodep1;
 
             this.size++;
         }
     }
 
-    public int getLast() throws Exception
-    {
-        if(this.size==0)
-        {
+    public int getLast() throws Exception {
+        if (this.size == 0) {
             throw new Exception("Emtpy Bro");
         }
         return tail.data;
     }
-    public int getFirst() throws Exception
-    {
-        if(this.size==0)
-        {
+
+    public int getFirst() throws Exception {
+        if (this.size == 0) {
             throw new Exception("Empty Bro");
         }
         return head.data;
     }
-    public int getAt(int idx) throws Exception
-    {
-        if(this.size==0)
-        {
+
+    public int getAt(int idx) throws Exception {
+        if (this.size == 0) {
             throw new Exception("Empty Bro");
-        }
-        else if(idx<0 || idx>=this.size)
-        {
+        } else if (idx < 0 || idx >= this.size) {
             throw new Exception("Out of Bounds");
         }
-        Node node=head;
-        for(int i=0;i<idx;i++)
-        {
-            node=node.next;
+        Node node = head;
+        for (int i = 0; i < idx; i++) {
+            node = node.next;
         }
         return node.data;
     }
 
-    public Node getNodeAt(int idx) throws Exception
-    {
-        if(this.size==0)
-        {
+    public Node getNodeAt(int idx) throws Exception {
+        if (this.size == 0) {
             throw new Exception("Empty Bro");
-        }
-        else if(idx<0 || idx>=this.size)
-        {
+        } else if (idx < 0 || idx >= this.size) {
             throw new Exception("Out of Bounds");
         }
-        Node node=head;
-        for(int i=0;i<idx;i++)
-        {
-            node=node.next;
+        Node node = head;
+        for (int i = 0; i < idx; i++) {
+            node = node.next;
         }
         return node;
     }
-    public static void main(String[] args) throws Exception{
-        linkedList list=new linkedList();
-        
+
+    public int removeFirst() throws Exception {
+        int temp = head.data;
+        if (this.size == 0) {
+            throw new Exception("Invalid");
+        } else if (this.size == 1) {
+
+            head = tail = null;
+            this.size--;
+            return temp;
+        } else {
+            Node second = head.next;
+            head = second;
+
+            this.size--;
+        }
+        return temp;
+    }
+
+    public int removeLast() throws Exception {
+        if (this.size == 0) {
+            throw new Exception("Invalid");
+        } else if (this.size == 1) {
+            int temp = head.data;
+            head = tail = null;
+            this.size--;
+            return temp;
+        } else {
+            int temp = tail.data;
+            Node secondLast = this.getNodeAt(this.size - 2);
+            tail = secondLast;
+            tail.next = null;
+            this.size--;
+            return temp;
+        }
+    }
+
+    public int removeAt(int idx) throws Exception {
+        if (this.size == 0) {
+            throw new Exception("Empty Bro");
+        } else if (idx < 0 || idx >= this.size) {
+            throw new Exception("Out of Bounds");
+        } else if (idx == 0) {
+            return this.removeFirst();
+        } else if (idx == this.size - 1) {
+            return this.removeLast();
+        } else {
+            Node nodem1=this.getNodeAt(idx-1);
+            Node n=nodem1.next;
+            Node nodep1=n.next;
+            nodem1.next=nodep1;
+
+            this.size--;
+            return n.data;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        linkedList list = new linkedList();
+
         list.addFirst(5);
         list.addLast(10);
         list.addLast(20);
         list.addLast(30);
         list.addLast(40);
         list.display();
-        list.addAtAny(100,2);
+        // list.addAtAny(100, 2);
+        list.removeAt(3);
         list.display();
     }
 }
