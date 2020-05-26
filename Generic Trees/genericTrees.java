619bf97ec449738b82f30222e28eed9357a7b74a
lakshaynanda <lakshay.nanda08@gmail.com>
2020-05-26 17:02:26 +0530

    Generic Tree Initialization

---
+++ Generic Trees/genericTrees.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class genericTrees {
+    private class Node
+    {
+        int data;
+        ArrayList<Node> children=new ArrayList<>();
+    }
+    private int size;
+    private Node root;
+}
