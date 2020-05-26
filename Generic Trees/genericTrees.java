c5d840a9ed65e3b50ec6c85c04a3c2264bd02aa4
lakshaynanda <lakshay.nanda08@gmail.com>
2020-05-26 18:59:59 +0530

    Tree Create

--- Generic Trees/genericTrees.java
+++ Generic Trees/genericTrees.java
@@ -7,4 +7,32 @@ public class genericTrees {
     }
     private int size;
     private Node root;
+    public genericTrees()
+    {
+        this.root=construct(new Scanner(System.in), null, 0);
+    }
+    private Node construct(Scanner sc,Node parent,int i)
+    {
+        if(parent==null)
+        {
+            System.out.println("Enter the data for Parent");
+        }
+        else
+        {
+            System.out.println("Enter the data for the"+i+"th child of"+parent);
+        }
+        int cdata=sc.nextInt();
+        Node child=new Node();
+        this.size++;
+        child.data=cdata;
+
+        System.out.println("Enter the number of children for"+child.data);
+        int numGrandChil=sc.nextInt();
+        for(int j=0;j<numGrandChil;j++)
+        {
+            Node granChil = construct(sc, child, j);
+            child.children.add(granChil);
+        }
+        return child;
+    }
 }
