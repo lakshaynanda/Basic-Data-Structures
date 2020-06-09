e5aa49eff6a7789a5468e665d9602ee34f503b20
lakshaynanda <lakshay.nanda08@gmail.com>
2020-06-09 20:18:59 +0530

    Display and Size

--- Generic Trees/genericTrees.java
+++ Generic Trees/genericTrees.java
@@ -11,8 +11,51 @@ public class genericTrees {
     {
         this.root=construct(new Scanner(System.in), null, 0);
     }
+    public int size()
+    {
+        return this.size;
+    }
+    public boolean isEmpty()
+    {
+        if(this.size==0)
+            return true;
+        else
+            return false;
+    }
+    public void display()
+    {
+        display(root);
+    }
+    private void display(Node node)
+    {
+        String str=node.data+" -> ";
+        for(Node child:node.children)
+        {
+            str+=child.data+", ";
+        }
+        str+=".";
+        System.out.println(str);
+        for(Node child:node.children)
+        {
+            display(child);
+        }
+    }
+    public int size2()
+    {
+        return size2(root);
+    }
+    public int size2(Node node)
+    {
+        int size=0;
+        for(Node child:node.children)
+        {
+            int csize=size2(child);
+            size += csize;
+        }
+        size += 1;
+        return size;
+    }
 
-    
     private Node construct(Scanner sc,Node parent,int i)
     {
         if(parent==null)
@@ -21,14 +64,14 @@ public class genericTrees {
         }
         else
         {
-            System.out.println("Enter the data for the"+i+"th child of"+parent);
+            System.out.println("Enter the data for the"+i+"th child of "+parent);
         }
         int cdata=sc.nextInt();
         Node child=new Node();
         this.size++;
         child.data=cdata;
 
-        System.out.println("Enter the number of children for"+child.data);
+        System.out.println("Enter the number of children for "+child.data);
         int numGrandChil=sc.nextInt();
         for(int j=0;j<numGrandChil;j++)
         {
@@ -36,5 +79,11 @@ public class genericTrees {
             child.children.add(granChil);
         }
         return child;
+    }
+    public static void main(String args[])
+    {
+        genericTrees tree=new genericTrees();
+        tree.display();
+        System.out.println(tree.size2());
     }
 }
