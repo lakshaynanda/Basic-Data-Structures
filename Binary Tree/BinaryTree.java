dd909d1aa5bf8fe669981275d7470eccd100d9fe
lakshaynanda <lakshay.nanda08@gmail.com>
2020-07-05 22:00:32 +0530

    Updatesssssssss

---
+++ Binary Tree/BinaryTree.java
@@ -0,0 +1,12 @@
+package binarytree;
+
+public class BinaryTree {
+    private class Node
+    {
+        int data;
+        Node left;
+        Node right;
+    }
+    private Node root;
+    private int size;
+}
