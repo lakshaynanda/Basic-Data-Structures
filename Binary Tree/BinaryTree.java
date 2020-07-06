c66786a9cfa5eaef77e40ff7b0b03b2b027c2d6c
lakshaynanda <lakshay.nanda08@gmail.com>
2020-07-06 19:36:27 +0530

    Tree Display

--- Binary Tree/BinaryTree.java
+++ Binary Tree/BinaryTree.java
@@ -1,5 +1,5 @@
-package binarytree;
-
+// package binarytree;
+import java.util.*;
 public class BinaryTree {
     private class Node
     {
@@ -9,4 +9,79 @@ public class BinaryTree {
     }
     private Node root;
     private int size;
+    public BinaryTree()
+    {
+        this.root=this.construct(new Scanner(System.in),null,false);
+    }
+    private Node construct(Scanner scn,Node parent,boolean ilc)
+    {   
+        if(parent==null)
+        {
+            System.out.println("Enter the data for root");
+        }
+        else{
+            if(ilc)
+            {
+                System.out.println("Enter the data for left child of "+parent.data);
+            }else
+            {
+                System.out.println("Enter the data for right child of "+parent.data);
+            }
+       }
+        int cdata=scn.nextInt();
+
+        Node child=new Node();
+        child.data=cdata;
+        this.size++;
+
+        System.out.println("Do you have a left child "+child.data);
+        boolean hlc=scn.nextBoolean();
+        if(hlc)
+        {
+            child.left=construct(scn,child,true);
+        }
+        System.out.println("Do you have a right child "+child.data);
+        boolean hrc=scn.nextBoolean();
+        if(hrc)
+        {
+            child.right=construct(scn,child,false);
+        }
+        return child;
+    }
+    public int size()
+    {
+        return this.size;
+    }
+    public boolean isEmpty()
+    {
+        return this.size()==0;
+    }
+    public void display()
+    {
+        display(root);
+    }
+    public void display(Node node)
+    {
+        if(node==null)
+        {
+            return;
+        }
+        String str="";
+        str+=node.left!=null?node.left.data:".";
+        str+=" -> " + node.data + " <- ";
+        str+=node.right!=null?node.right.data:".";
+        System.out.println(str);
+
+        display(node.left);
+        display(node.right);
+    }
+
+    public static void main(String args[])
+    {
+        BinaryTree tree=new BinaryTree();
+        // System.out.println(tree.size());
+        tree.display();
+        //50 true 25 true 12 false true 20 false false true 37 true 30 false false false true 75 true 62 false false true 87 false false
+    }
+
 }
