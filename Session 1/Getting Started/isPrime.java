799871b965849c68fc5e9f3b3465831348cfb9b5
lakshaynanda <lakshay.nanda08@gmail.com>
2021-05-15 23:55:07 +0530

    updated isPrime

--- Session 1/Getting Started/isPrime.java
+++ Session 1/Getting Started/isPrime.java
@@ -1,26 +1,28 @@
 import java.util.*;
   
-  public class Main{
+  public class isPrime{
   
   public static void main(String[] args) {
       Scanner scn = new Scanner(System.in);
       int t = scn.nextInt();
-      
-      for(int i = 0; i < t; i++) {
-        int n = scn.nextInt();
-        int count = 0;
-        for(int div = 2; div*div <= n; div++) {
-            if (n % div == 0) {
-                count++;
+      if (t == 1 || t == 0) {
+          System.out.println("not prime");
+      } else {
+        for(int i = 0; i < t; i++) {
+            int n = scn.nextInt();
+            int count = 0;
+            for(int div = 2; div*div <= n; div++) {
+                if (n % div == 0) {
+                    count++;
+                }
             }
-        }
-        if (count == 0) {
-            System.out.println("prime");
-        } else {
-           System.out.println("not prime"); 
+            if (count == 0) {
+                System.out.println("prime");
+            } else {
+            System.out.println("not prime"); 
+            }
         }
       }
        // write ur code here
-  
    }
   }
