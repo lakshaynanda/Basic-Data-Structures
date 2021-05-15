078f0dc3c2c2f0101469a970c7c45e4d210d26b2
lakshaynanda <lakshay.nanda08@gmail.com>
2021-05-15 22:14:04 +0530

    pattern 13

---
+++ Patterns/pattern13.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        //write your code here
+        for(int i = 0; i < n; i++) {
+            int icj = 1;
+            for(int j = 0; j <= i; j++) {
+                System.out.print(icj +"	");
+                int icjp1 = (icj*(i-j))/(j+1);
+                icj = icjp1;
+            }
+            System.out.println();
+        }
+    }
+}
