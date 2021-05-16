feeebcc3678b4be621051d11e7eabb17c632cd15
lakshaynanda <lakshay.nanda08@gmail.com>
2021-05-16 21:13:37 +0530

    span of an array

--- Session 1/Arrays and Functions/spanArray.java
+++ Session 1/Arrays and Functions/spanArray.java
@@ -12,13 +12,11 @@ public static void main(String[] args) throws Exception {
         arr[i] = sc.nextInt();
     }
     int max = Integer.MIN_VALUE;
+    int min = Integer.MAX_VALUE;
     for(int i = 0; i < arr.length; i++) {
         if (arr[i] > max) {
             max = arr[i];
         }
-    }
-    int min = Integer.MAX_VALUE;
-    for (int i = 0; i < arr.length; i++) {
         if (arr[i] < min) {
             min = arr[i];
         }
