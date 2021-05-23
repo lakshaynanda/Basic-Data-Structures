beeff6615439234b5d4eeaff9c7c43c3cf17dbee
lakshaynanda <lakshay.nanda08@gmail.com>
2021-05-24 02:44:01 +0530

    subarrays

---
+++ Session 1/Arrays and Functions/subarrays.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int arr[] = new int[n];
+    for(int i = 0; i < arr.length; i++) {
+        arr[i] = sc.nextInt();
+    }
+    for(int i = 0; i < arr.length; i++) {
+        for(int j = i + 1; j <= arr.length; j++) {
+            for(int k = i; k < j; k++){
+                System.out.print(arr[k] + "\t" );
+            }
+            System.out.println();
+        }
+    }
+ }
+
+}
