2b08ad1536b1ca0930dcf3facd0474030dc7a57e
lakshaynanda <lakshay.nanda08@gmail.com>
2021-05-24 02:45:07 +0530

    subarrays

--- Session 1/Arrays and Functions/subarrays.java
+++ Session 1/Arrays and Functions/subarrays.java
@@ -12,8 +12,8 @@ public static void main(String[] args) throws Exception {
         arr[i] = sc.nextInt();
     }
     for(int i = 0; i < arr.length; i++) {
-        for(int j = i + 1; j <= arr.length; j++) {
-            for(int k = i; k < j; k++){
+        for(int j = i; j < arr.length; j++) {
+            for(int k = i; k <= j; k++){
                 System.out.print(arr[k] + "\t" );
             }
             System.out.println();
